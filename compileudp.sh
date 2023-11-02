97a914cb30586a82f0d08d20ab8ee76ca3524810
Joshua Burdick <joshburdick1@hotmail.com>
2023-11-02 18:15:46 -0400

    Created structure for UDP implementation

--- compileudp.sh
+++ compileudp.sh
@@ -2,7 +2,7 @@
 
 # Compile the program
 cd server
-g++ -std=c++11 -o udpserver udpdriver.cpp TCPServer.cpp
+g++ -std=c++11 -o udpserver ../executables/udpdriver.cpp UDPServer.cpp
 
 cd ../client
-g++ -std=c++11 -o udpserver udpdriver.cpp TCPClient.cpp
+g++ -std=c++11 -o udpserver ../executables/udpdriver.cpp UDPClient.cpp
